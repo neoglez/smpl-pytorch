ee1f63dd085f96e4d669ac3698e2c721a15a4561
neoglez <neoglez@users.noreply.github.com>
2019-07-17 09:32:48 +0200

    resolve conflicts

--- setup.py
+++ setup.py
@@ -9,13 +9,8 @@ REQUIREMENTS = [
     "numpy"]
 
 setuptools.setup(
-<<<<<<< HEAD
-    name="smpl-pytorch",
-    version="0.0.7",
-=======
     name="smplpytorch",
-    version="0.0.1",
->>>>>>> a18b0197d8a7204865b2ec95307e2286533f9487
+    version="0.0.7",
     author="Gul Varol",
     author_email="gulvarols@gmail.com",
     python_requires=">=3.5.0",
